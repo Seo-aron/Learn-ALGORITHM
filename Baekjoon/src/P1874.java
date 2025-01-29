ffbc553c743e5a4b4f3f7f168bc78a0eeb382a54
Seo-aron <dkfhs4337@naver.com>
2025-01-29 22:40:47 +0900

    1874

--- Baekjoon/src/P1874.java
+++ Baekjoon/src/P1874.java
@@ -1,7 +1,33 @@
 import java.util.Scanner;
+import java.util.Stack;
 
 public class P1874 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int num = 0;
+        Stack<Integer> stack = new Stack<Integer>();
+        int lastPush = 0;
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < n; i++){
+            num = sc.nextInt();
+
+            if (lastPush < num) {
+                for (int j = lastPush + 1; j <= num; j++) {
+                    stack.push(j);
+                    result.append("+\n");
+                }
+                lastPush = num;
+            }
+            if(stack.peek() == num){
+                stack.pop();
+                result.append("-\n");
+            } else {
+                System.out.println("NO");
+                return;
+            }
+        }
+        System.out.println(result);
     }
 }
