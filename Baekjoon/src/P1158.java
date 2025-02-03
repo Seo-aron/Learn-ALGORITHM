0fdecbf6770076800145876f9464c03d449f2e2d
Seo-aron <dkfhs4337@naver.com>
2025-02-03 16:31:14 +0900

    P1158

--- Baekjoon/src/P1158.java
+++ Baekjoon/src/P1158.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
 
 public class P1158 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int person = Integer.parseInt(input[0]);
+        int cnt = Integer.parseInt(input[1]);
+
+        List<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= person; i++) {
+            queue.add(i);
+        }
+    }
 }
