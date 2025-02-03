bba4ab0e63f591e7e5cbec343b3042d0e65b0f7f
Seo-aron <dkfhs4337@naver.com>
2025-02-03 19:16:49 +0900

    P1158

--- Baekjoon/src/P1158.java
+++ Baekjoon/src/P1158.java
@@ -10,10 +10,24 @@ public class P1158 {
         String[] input = br.readLine().split(" ");
         int person = Integer.parseInt(input[0]);
         int cnt = Integer.parseInt(input[1]);
+        int current = 0;
 
         List<Integer> queue = new LinkedList<>();
         for (int i = 1; i <= person; i++) {
             queue.add(i);
         }
+
+        StringBuilder result = new StringBuilder();
+        result.append("<");
+
+        while(!queue.isEmpty()){
+            current = (current + cnt - 1) % queue.size();
+            result.append(queue.remove(current));
+            if(!queue.isEmpty()){
+                result.append(", ");
+            }
+        }
+        result.append(">");
+        System.out.println(result);
     }
 }
