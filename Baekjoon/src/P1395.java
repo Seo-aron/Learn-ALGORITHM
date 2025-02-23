d6d890e9e179f81cfd6aa438270278c52cbf303c
Seo-aron <dkfhs4337@naver.com>
2025-02-24 00:10:47 +0900

    1395

---
+++ Baekjoon/src/P1395.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P1395 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int length = Integer.parseInt(br.readLine());
+        String notation = br.readLine();
+        double[] result = new double[length];
+    }
+}
