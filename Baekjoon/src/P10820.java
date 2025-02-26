2de8f0e5deeff54abb5778a0c4a5bb2972629f5d
Seo-aron <dkfhs4337@naver.com>
2025-02-27 05:57:14 +0900

    10820

--- Baekjoon/src/P10820.java
+++ Baekjoon/src/P10820.java
@@ -1,4 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class P10820 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        // 출력 소문자, 대문자, 숫자, 공백
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+
+        while ((str = br.readLine()) != null){
+            int[] result = new int[4];
+
+            for (char ch : str.toCharArray()){
+                if(ch >= 'a' && ch <= 'z'){
+                    result[0] += 1;
+                } else if(ch >= 'A' && ch <= 'Z'){
+                    result[1] += 1;
+                } else if(ch >= '0' && ch <= '9'){
+                    result[2] += 1;
+                } else if(ch == ' '){
+                    result[3] += 1;
+                }
+            }
+            System.out.println(result[0] + " " + result[1] + " " + result[2] + " " + result[3]);
+        }
     }
 }
