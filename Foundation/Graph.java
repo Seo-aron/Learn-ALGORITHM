ed0661c36e0893cfb2a498e8c1093f7e6d0907f8
Seo-aron <dkfhs4337@naver.com>
2025-03-08 19:56:34 +0900

    BFS, DFS

---
+++ Foundation/Graph.java
@@ -0,0 +1,13 @@
+/*
+    DFS(Depth-First Search), BFS(Breadth-First Search)
+    
+    - DFS (깊이 우선 탐색) : 그래프에서 한 노드에서 출발하여 갈 수 있는 곳까지 한 방향으로 탐색한 후,
+                           더 이상 갈 곳이 없으면 되돌아가면서 탐색하는 방식 (재귀 또는 스택 사용)
+    
+    - BFS (너비 우선 탐색) : 시작 노드에서 가까운 노드부터 탐색하며, 한 레벨씩 넓혀가면서 탐색하는 방식 (큐 사용)
+ */
+ public class Graph {
+    public static void main(String[] args) {
+        
+    }
+ }
