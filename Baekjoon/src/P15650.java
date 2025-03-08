78f7ca165b7d6a3c304b215561430353cdd5b744
Seo-aron <dkfhs4337@naver.com>
2025-03-08 18:24:45 +0900

    15650

---
+++ Baekjoon/src/P15650.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class P15650 {
+    public static void combination(int[] arr, int m, List<Integer> result, int start){
+        if (result.size() == m){
+            for (int i = 0; i < result.size(); i++){
+                System.out.print(result.get(i) + " ");
+            }
+            System.out.println();
+            return;
+        }
+        for (int i = start; i < arr.length; i++){
+            result.add(arr[i]);
+            combination(arr, m, result, i + 1);
+            result.remove(result.size() - 1);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();   // 1~n까지 범위
+        int m = sc.nextInt();   // 몇개 뽑을건지
+        int[] arr = new int[n];
+        boolean[] visited = new boolean[n];
+
+        for (int i = 0; i < n; i++){
+            arr[i] = i + 1;
+        }
+
+        combination(arr, m, new ArrayList<>(), 0);
+
+        sc.close();
+    }
+}
