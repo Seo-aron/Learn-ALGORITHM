af499348dd6d8990a91f7830024331ec8a6879e3
Seo-aron <dkfhs4337@naver.com>
2025-05-15 16:07:21 +0900

    휴게소 세우기

--- Baekjoon/src/P1477.java
+++ Baekjoon/src/P1477.java
@@ -6,7 +6,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main {
+public class P1477 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
